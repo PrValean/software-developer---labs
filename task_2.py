00d5329160f0e8b38c7ffb180f2a3509c0878f39
PrValean <145262194+PrValean@users.noreply.github.com>
2024-10-23 00:07:37 +0300

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,3 +1,4 @@
+# TODO Найдите количество книг, которое можно разместить на дискете
 size_floppy_in_Mb = 1.44
 size_floppy = 1.44 * 1024 * 1024
 
@@ -8,5 +9,5 @@ weight_of_symbol = 4
 
 size_of_book = weight_of_symbol * numbers_of_symbol * numbers_of_string * numbers_of_page
 
-count_of_book = round(size_floppy / size_of_book)
+count_of_book = int(size_floppy // size_of_book)
 print("Количество книг, помещающихся на дискету:", count_of_book)
