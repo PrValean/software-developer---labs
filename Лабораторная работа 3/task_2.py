c59ffb5157282429269bd0c974cc684884f2eba8
PrValean <145262194+PrValean@users.noreply.github.com>
2024-11-08 02:06:27 +0300

    Update task_2.py

--- Лабораторная работа 3/task_2.py
+++ Лабораторная работа 3/task_2.py
@@ -13,4 +13,8 @@ participants_first_group = "Иванов|Петров|Сидоров"
 participants_second_group = "Петров|Сидоров|Смирнов"
 
 # TODO Провеьте работу функции с разделителем отличным от запятой
-print(find_common_participants(participants_first_group, participants_second_group, "|"))
+print(find_common_participants(
+    participants_first_group,
+    participants_second_group,
+    "|"
+))
